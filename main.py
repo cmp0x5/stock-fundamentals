1af47f442e317b848174f8d6ed815a4d995244f6
Victor <camposvictor035@gmail.com>
2024-11-11 16:47:04 -0300

    ctftime update

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 
 from telegram import Update, ForceReply
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from handlers.rssparse import ctf_time_update
 
 logging.basicConfig(
         format="%(asctime)s | %(name)s | %(levelname)s | %(message)s", level=logging.INFO
@@ -30,18 +31,19 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("No Lmao")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(update.message.text + "?")
-    await update.message.reply_text("what is bro waffling about")
+    await update.message.reply_text(update.message.text)
 
 def main() -> None:
     application = Application.builder().token(token).build()
     
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("ctf", ctf_time_update))
     application.add_handler(CommandHandler("help", help))
 
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
+
 
 if __name__ == '__main__':
     main()
