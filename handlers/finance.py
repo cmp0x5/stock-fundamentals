e35e9c2597a05e199fbcf4c90f3fd8e29d5301d1
Victor <camposvictor035@gmail.com>
2024-12-23 14:59:26 -0300

    stock analysis

--- handlers/finance.py
+++ handlers/finance.py
@@ -22,6 +22,45 @@ async def get_ticker_history(update: Update, context: ContextTypes.DEFAULT_TYPE)
         exchange = ticker.info["exchange"]
         currency = ticker.info["currency"]
 
+        currentPrice = ticker.info["currentPrice"]
+        netIncomeToCommon = ticker.info["netIncomeToCommon"]
+        trainingEps = ticker.info["trailingEps"]
+        returnOnEquity = ticker.info["returnOnEquity"] * 100
+        profitMargins = ticker.info["profitMargins"] * 100
+        revenueGrowth = ticker.info["revenueGrowth"] * 100
+        earningsGrowth = ticker.info["earningsGrowth"] * 100
+        debtToEquity = ticker.info["debtToEquity"]
+        currentRatio = ticker.info["currentRatio"]
+        dividendYield = ticker.info["dividentYield"] * 100
+        payoutRatio = ticker.info["payoutRatio"] * 100
+        priceToEarnings = currentPrice / trailingEps
+        priceToBook = ticker.info["priceToBook"]
+        freeCashFlow = ticker.info["freeCashflow"]
+
+        await update.message.reply_text(f"""
+            Current Price: {currentPrice} {currency} 
+            \nResults for fiscal year {ticker.info["fiscalYear"]}:
+            \nProfitability:
+            \nNet Income To Common Shareholders: {netIncomeToCommon} {currency} 
+            \nEarnings Per Share: {trailingEps} {currency} 
+            \nReturn on Equity: {returnOnEquity}%
+            \nProfit Margin: {profitMargins}%
+            \nGrowth Indicators:
+            \nRevenue Growth: {revenueGrowth}%
+            \nEarnings Growth: {earningsGrowth}%
+            \nFinancial Health:
+            \nDebt to Equity: {debtToEquity}%
+            \nCurrent Ratio: {currentRatio} 
+            \nDivident Health:
+            \nDividend Yield: {dividendYield}%
+            \nPayout Ratio: {payoutRatio}%
+            \nValuation Metrics:
+            \nP/E: {priceToEarnings}
+            \nP/B: {priceToBook}
+            \nCash Flow:
+            \nFree Cash Flow: {freeCashFlow} {currency} 
+        """)
+
         var = (data["Close"].iloc[-1] / data["Close"].iloc[0] - 1) * 100
         if (var > 0):
             sign = "+"
@@ -51,5 +90,6 @@ async def get_ticker_history(update: Update, context: ContextTypes.DEFAULT_TYPE)
         plt.close()
         with open(f"chart.jpg", "rb") as chart:
             await update.message.reply_photo(photo=chart, caption=f"{name} values over time")
+
     except Exception as e:
         print(f"Error: {e}")
